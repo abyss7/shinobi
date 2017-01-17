de8f3e798b4d2b648b1de1457868e630447971d2
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2017-01-17 18:21:20 +0300

    Bundle gflags.
    Also add '--data' argument to unit-tests binary.

---
+++ src/language/shi/lexer_test.cc
@@ -0,0 +1,18 @@
+#include <language/shi/lexer.hh>
+
+// Third-party
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+
+namespace shinobi {
+
+DECLARE_string(data);
+
+namespace language::shi {
+
+TEST(LexerShi, DISABLED_GoodSample1) {
+  // TODO: create some typical examples of parseable language constructions.
+}
+
+}  // namespace language::shi
+}  // namespace shinobi
--- src/test/main.cc
+++ src/test/main.cc
@@ -1,7 +1,28 @@
-#include <third_party/gtest/exported/googletest/include/gtest/gtest.h>
+#include <base/aliases.hh>
+#include <base/logging.hh>
+
+// Third-party
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+
+#include <base/using_log.hh>
+
+namespace shinobi {
+DEFINE_string(data, String(), "Path to the external data for tests");
+// Declare this flag in unit-test source files - where needed.
+}
 
 int main(int argc, char* argv[]) {
+  // TODO: setup logging.
+
   ::testing::InitGoogleTest(&argc, argv);
-  ::testing::FLAGS_gtest_death_test_style = "fast";
+  ::testing::GTEST_FLAG(death_test_style) = "fast";
+  ::testing::GTEST_FLAG(show_internal_stack_frames) = false;
+
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  if (shinobi::FLAGS_data.empty()) {
+    LOG(ERROR) << "No data path specified - some tests may fail!";
+  }
   return RUN_ALL_TESTS();
 }
